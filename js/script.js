673729c8a8bcfcc9f2ad58ce220e3eb6a257bfba
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-03-13 23:33:50 +0600

    Make Lorem ipsum app

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// document.getElementById('input').addEventListener('keyup', function(event) {
+//     let text = document.getElementById('input').value;
+//     let times = "";
+//     for (let index = 0; index < text.length; index++) {
+//         if (typeof parseInt(text[index]) == Number) {
+//             console.log(true)
+//         } else {
+//             console.log(false)
+
+//         }
+
+
+//     }
+//     // console.log(event.key)
+// })
+const showFromApi = (data, times) => {
+    const loremTextData = data[0].text;
+    let loremArrayData = loremTextData.split(" ");
+    document.getElementById('output').textContent = "";
+    if (loremArrayData.length < times) {
+        document.getElementById('output').innerText = "Enter numbers between 1 to 10000";
+    } else {
+        let displayText = "";
+        for (let index = 0; index < times; index++) {
+            displayText += loremArrayData[index] + " ";
+        }
+        document.getElementById('output').innerText = displayText;
+    }
+}
+const getText = (times) => {
+    fetch("../data/data.json")
+        .then(res => res.json())
+        .then(data => showFromApi(data, times))
+
+}
+document.getElementById('enter').addEventListener('click', () => {
+    let times = document.getElementById('input').value;
+    const displayText = getText(times);
+
+});
